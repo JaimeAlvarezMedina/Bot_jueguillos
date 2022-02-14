e9797ab988aec11744d5d2e3794626bc0971619d
--replace-all <jaime.jim8@gmail.com>
2022-02-14 19:50:36 +0100

    puto

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ function getNoticias($chatId){
     $array = json_decode($json, TRUE); 
   
     for ($i=0; $i < 9; $i++) {  
-        $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>"; 
+        $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title'].$array['channel']['item'][$i]['link']; 
     } 
   
     sendMessage($chatId, $titulos); 
