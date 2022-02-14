c8c18e6fb96fad9f24d3fb1fb3f34d491f6a49e5
--replace-all <jaime.jim8@gmail.com>
2022-02-14 16:28:40 +0100

    make it better

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$token = '5296594085:AAHNqJohJUg2WAF_yCooXDx6P3TPOzD6ouE';
+$website = 'https://api.telegram.org/bot'.$token;
+
+$input = file_get_contents('php://input');
+$update = json_decode($input, TRUE);
+
+$chatId = $update['message']['chat']['id'];
+$message = $update['message']['text'];
+
+switch($message) {
+    case '/start':
+        $response ='iniciado';
+        sendMessage($chatId, $response);
+        break;
+    case '/cosa':
+        switch($num){
+            case 1:
+                $response = 'puto';
+                break;
+        }
+        
+        sendMessage($chatId, $response);
+        break;
+    
+    default:
+        $response = 'Introduce uno de los comandos, no intentes explorar';
+        sendMessage($chatId, $response);
+        break;
+}
+
+function sendMessage($chatId, $response) {
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    file_get_contents($url);
+}
+?>
