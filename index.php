9a2a70efe72e5c0c41a8fd1d139a8acac028a8b9
--replace-all <jaime.jim8@gmail.com>
2022-02-14 19:40:05 +0100

    puto

--- index.php
+++ index.php
@@ -1,49 +1,54 @@
-<?php
-$token = '5296594085:AAHNqJohJUg2WAF_yCooXDx6P3TPOzD6ouE';
-$website = 'https://api.telegram.org/bot'.$token;
-
-$input = file_get_contents('php://input');
-$update = json_decode($input, TRUE);
-
-$chatId = $update['message']['chat']['id'];
-$message = $update['message']['text'];
-
-switch($message) {
-    case '/start':
-        $response ='iniciado';
-        sendMessage($chatId, $response);
-        break;
-    case '/juegos':
-        buscar_juegos($chatId);
-        break;
-    
-    default:
-        $response = 'Introduce uno de los comandos, no intentes explorar';
-        sendMessage($chatId, $response);
-        break;
-}
-
-function sendMessage($chatId, $response) {
-    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
-    file_get_contents($url);
-}
-
-function buscar_juegos($chatId){
-    $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
-    $enlace="https://zara2.webnode.es/rss/all.xml";
-
-    $xmlstring = file_get_contents($enlace,false,$context);
-
-    $xml= simplexml_load_string($xmlstring, "SimpleXMLElement",LIBXML_NOCDATA);
-    $json=json_encode($xml);
-    $array=json_decode($json,TRUE);
-    
-    for ($i=0; $i < 9; $i++) { 
-        $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
-    }
-
-    sendMessage($chatId, $titulos);
-
-}
-
+<?php 
+$token = '5296594085:AAHNqJohJUg2WAF_yCooXDx6P3TPOzD6ouE'; 
+$website = 'https://api.telegram.org/bot'.$token; 
+ 
+$input = file_get_contents('php://input'); 
+$update = json_decode($input, TRUE); 
+ 
+$chatId = $update['message']['chat']['id']; 
+$message = $update['message']['text']; 
+ 
+switch($message) { 
+    case '/start': 
+        $response = 'Me has iniciado'; 
+        sendMessage($chatId, $response); 
+        break; 
+    case '/noticias': 
+            getNoticias($chatId); 
+        break; 
+    case '/info': 
+        $response = 'Hola! Soy @ruben'; 
+        sendMessage($chatId, $response); 
+        break; 
+    default: 
+        $response = 'No te he entendido'; 
+        sendMessage($chatId, $response); 
+        break; 
+} 
+ 
+function sendMessage($chatId, $response) { 
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response); 
+    file_get_contents($url); 
+} 
+ 
+function getNoticias($chatId){   
+  
+    $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml'))); 
+    $url = "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada"; 
+  
+    $xmlstring = file_get_contents($url, false, $context); 
+  
+    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA); 
+    $json = json_encode($xml); 
+    $array = json_decode($json, TRUE); 
+  
+    for ($i=0; $i < 9; $i++) {  
+        $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>"; 
+    } 
+  
+    sendMessage($chatId, $titulos); 
+ 
+} 
+ 
+ 
 ?>
