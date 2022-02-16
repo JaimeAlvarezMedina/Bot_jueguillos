e65cd85cf221808beb28a557cecf93fa606d2f80
--replace-all <jaime.jim8@gmail.com>
2022-02-16 15:25:41 +0100

    puto

--- index.php
+++ index.php
@@ -7,22 +7,31 @@ $update = json_decode($input, TRUE);
  
 $chatId = $update['message']['chat']['id']; 
 $message = $update['message']['text']; 
-$reply=$update['message']['reply_to_message']['text']; 
-
+$repl=$update['message']['reply_to_message']['text']; 
  
 switch($message) { 
     case '/start': 
-        $response = 'Iniciando...'; 
+        $response = 'Me has iniciado'; 
         sendMessage($chatId, $response,True); 
         break; 
-
-    case '/juegos': 
-        obtener_juegos($chatId); 
+    case '/video': 
+        $response = 'https://vdmedia.elpais.com/elpaistop/multimedia/20222/13/20220213235721471_1644793109_video_1800.mp4'; 
+        sendVideo($chatId, $response,True); 
         break; 
-
+    case '/audio': 
+        $response = 'https://www.youtube.com/watch?v=wlPl-rP6j2g'; 
+        sendAudio($chatId, $response); 
+        break;     
+    case '/noticias': 
+        getNoticias($chatId); 
+        break; 
+    case '/info': 
+        $response = 'Hola! Soy @ruben'; 
+        sendMessage($chatId, $response,True); 
+        break; 
     default: 
-        $response = 'Aprende los comandos, no hay easter-egg'; 
-        sendMessage($chatId, $response,false); 
+        $response = 'No te he entendido'; 
+        sendMessage($chatId, $response); 
         break; 
 } 
  
@@ -31,16 +40,23 @@ function sendMessage($chatId, $response, $repl) {
         $reply_mark=array('force_reply'=>True); 
         $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($response); 
     } 
-    else{
-        $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response); 
-    }
-        
+    else $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response); 
+    file_get_contents($url); 
 } 
  
-function obtener_juegos($chatId){   
+ 
+function sendVideo($chatId, $response) { 
+    $url = $GLOBALS['website'].'/sendVideo?chat_id='.$chatId.'&parse_mode=HTML&video='.urlencode($response); 
+    file_get_contents($url); 
+} 
+function sendAudio($chatId, $response) { 
+    $url = $GLOBALS['website'].'/sendAudio?chat_id='.$chatId.'&parse_mode=HTML&audio='.urlencode($response); 
+    file_get_contents($url); 
+} 
+function getNoticias($chatId){   
   
     $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml'))); 
-    $url = "https://www.metacritic.com/rss/games/pc"; 
+    $url = "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada"; 
   
     $xmlstring = file_get_contents($url, false, $context); 
   
@@ -48,10 +64,12 @@ function obtener_juegos($chatId){
     $json = json_encode($xml); 
     $array = json_decode($json, TRUE); 
   
-    for ($i=0; $i < 20; $i++) {  
-        $titulos ="\n\n".$array['channel']['item'][$i]['title'].$array['channel']['item'][$i]['link']; 
-        sendMessage($chatId, $titulos,false);
-    } 
+     
+        $titulos = $titulos."\n\n".$array['channel']['item'][0]['title'].$array['channel']['item'][0]['media:url']; 
+     
+  
+    sendVideo($chatId, $titulos); 
+ 
 } 
  
  
