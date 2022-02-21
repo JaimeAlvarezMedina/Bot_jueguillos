9a7d0e82f96208be71c85fc37f978c0529edae96
--replace-all <jaime.jim8@gmail.com>
2022-02-21 16:16:57 +0100

    puto

--- index.php
+++ index.php
@@ -11,18 +11,7 @@ $reply=$update['message']['reply_to_message']['text'];
 $replay=explode(" ",$reply);
 
 
-teclado(){
-    $keyboard = array('keyboard' =>
-            array(array(
-                array('text'=>'/juegos','callback_data'=>"1"),
-            ),
-                array(
-                    array('text'=>'/easter egg','callback_data'=>"4")
-                )), 'one_time_keyboard' => false, 'resize_keyboard' => true
-        );
-        file_get_contents('https://api.telegram.org/bot5157086336:AAGTbyTWlsvjqYuY1cTKkYAhzGEq11EQsIk/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($keyboard).'&text=Elija que desea hacer');
-         
-}
+
  
 teclado();
 
@@ -160,6 +149,16 @@ function obtener_juegos($chatId, $plataforma){
     } 
 } 
 
-
+teclado(){
+    $keyboard = array('keyboard' =>
+            array(array(
+                array('text'=>'/juegos','callback_data'=>"1"),
+            ),
+                array(
+                    array('text'=>'/easter egg','callback_data'=>"4")
+                )), 'one_time_keyboard' => false, 'resize_keyboard' => true
+        );
+        file_get_contents('https://api.telegram.org/bot5157086336:AAGTbyTWlsvjqYuY1cTKkYAhzGEq11EQsIk/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($keyboard).'&text=Elija que desea hacer');
+}
 
 ?>
