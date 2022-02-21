753cffa35c91fad895bb0c617f51583038a1a784
--replace-all <jaime.jim8@gmail.com>
2022-02-21 15:43:58 +0100

    puto

--- index.php
+++ index.php
@@ -24,10 +24,16 @@ if(empty($reply)){
         $response='¿De que plataforma quieres las criticas?';
         sendMessage($chatId, $response,TRUE); 
         break;
-    case '/easter egg':
+    case '/easter-egg':
         $response='Que no hay nada curiosona';
         sendMessage($chatId, $response,false); 
         break;
+    case '/help':
+        $response='Las plataformas disponibles son: \n\n PC \n\n PS3';
+        sendMessage($chatId, $response,false); 
+        break;
+
+
     default: 
         $response = 'Aprende los comandos, no hay easter-egg'; 
         sendMessage($chatId, $response, FALSE); 
@@ -153,7 +159,7 @@ function obtener_juegos($chatId, $plataforma){
             array('text'=>'/juegos','callback_data'=>"1"),
         ),
             array(
-                array('text'=>'/easter egg','callback_data'=>"4")
+                array('text'=>'/easter-egg','callback_data'=>"4")
             )), 'one_time_keyboard' => false, 'resize_keyboard' => true
         );
         file_get_contents('https://api.telegram.org/bot5157086336:AAGTbyTWlsvjqYuY1cTKkYAhzGEq11EQsIk/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($keyboard));
